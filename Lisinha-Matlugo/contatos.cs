8891169ca9fb916d3cd430fb137ccabf6096b55f
matlugo <matheuslugo2012@gmail.com>
2023-05-04 15:26:04 -0300

    Create contatos.cs

---
+++ Lisinha-Matlugo/contatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisinha_Matlugo
+{
+    internal class contatos
+    {
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone;
+    
+    //PROPRIEDADES (GET e SET)
+    public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+    public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+        public string Tlefone
+        { 
+            get { return telefone; } 
+            set { 
+                if (value.Length == 11)
+                
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "00-00000-0000"
+                }
+            
+            }
+
+
+        public contatos()
+        {
+        PrimeiroNome = "José"
+        Sobrenome = "Da Silva"
+        Telefone = "11-98506-6969"
+        }
+        
+        public contatos(string primeironome, string sobrenome, string telefone)
+        { primeiroNome = primeironome;
+        Sobrenome = sobrenome;
+        Telefone = telefone
+                }
+
+        public override string ToString()
+        {
+            string saida = String.Empty;
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
